85625422d2327374f3173dbed6fb6e005d59d149
Joel Gonzalez Fernandez <joel.gonzalez5115@gmail.com>
2024-10-17 11:54:19 -0300

    Estructura

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Page Title</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
+    <script src='main.js'></script>
+
+</head>
+
+<body>
+    <header>
+        <h1>My first PHP page</h1>
+        <nav>
+            <ul>
+                <li><a href="index.php">PREMIO</a></li>
+                <li><a href="about.php">INGRESAR</a></li>
+                <li><a href="contact.php">REGISTRAR</a></li>
+            </ul>
+        </nav>
+    </header>
+    <nav>
+        <ul>
+            <li><a href="index.php">En Vivo</a></li>
+            <li><a href="about.php">About</a></li>
+            <li><a href="contact.php">Contact</a></li>
+        </ul>
+    </nav>
+    <main>
+        <h2>Welcome to my first PHP page</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit
+    </main>
+</body>
+
+</html>
